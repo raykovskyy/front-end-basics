5c270d331e0dd1665769ff1a6e4e369befc6b7c6
raykovskyy <yuriy.raykovskyy@ria.com>
2018-07-10 18:09:18 +0300

    push sides

--- static/index.js
+++ static/index.js
@@ -1,13 +1,11 @@
-function createItem(data, side) {
-  var data = {
-    "author": "Джо Аберкромби",
-    "name": "Кровь и железо",
-    "img": "http://fantasy-worlds.org/img/preview/129/12974.jpg"
-  },
-  navArrow = side==="right"?"before":"after";
+var dataJSON;
+function createItem(id, data, side) {
+  var navArrow = side==="right"?"before":"after",
+    pushSide = side==="right"?"left":"right";
 
   let divItem = document.createElement('div');
     divItem.className = "item";
+    divItem.setAttribute("id", id);
 
     let divPic = document.createElement('div');
       divPic.className = "pic";
@@ -30,8 +28,8 @@ function createItem(data, side) {
 
     let divNav = document.createElement('div');
       divNav.className = navArrow;
+      divNav.addEventListener("click", (e)=>onArrowClick(e, pushSide));
 
-        
     spanImg.appendChild(img);
     divPic.appendChild(spanImg);
     divItem.appendChild(divPic);
@@ -45,18 +43,45 @@ function createItem(data, side) {
     divItem.appendChild(divTitle);
     divItem.appendChild(divNav);
 
-    // var leftPart = document.querySelector(`.${side}`);
-    // leftPart.insertBefore(divItem, leftPart.firstChild)
     return divItem;
+}
 
+function onArrowClick(e, pushSide){
+  var item = e.target.parentNode,
+    id = item.getAttribute('id'),
+    pushTo = document.querySelector(`.${pushSide=="right"?"right":"left"}`);
+    item.remove();
+    console.log(pushSide)
+    pushTo.appendChild(createItem(id, dataJSON[id], pushSide));
+}
+
+function readFile(path, callback){
+    var json = {},
+      path = path || 'static/data.json',
+      request = new XMLHttpRequest();
+      request.open("GET", path, false); request.send(null);
+    try{
+      json = JSON.parse(request.responseText);
+    }catch(err){console.error(err)};
+
+    return json;
 }
 
 function init(){
-  if(localStorage){
-
+  dataJSON = readFile();
+  if(localStorage.items){
+    Object.keys(localStorage.items).forEach(function(key){
+      let side = key,
+        div = document.querySelector(`.${side}`);
+      items[key].map((e)=> div.appendChild(createItem(e,dataJSON[e],side)));
+    });
+  }else{
+    let side = "left",
+      div = document.querySelector(`.${side}`);
+    Object.keys(dataJSON).map((e)=> div.appendChild(createItem(e,dataJSON[e],side)));
   }
 }
 
 document.addEventListener("DOMContentLoaded", function(){
-
+  init();
 });
